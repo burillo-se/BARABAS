bc9fb3eb55b96081783528660bb9b2784d72301a
burillo-se <spam@trap.com>
2019-04-21 14:07:55 +0100

    Tag version 1.7-pre5

--- BARABAS.cs
+++ BARABAS.cs
@@ -25,7 +25,7 @@ namespace SpaceEngineers
 #endif
         #endregion
         /*
-         * BARABAS v1.7-pre4
+         * BARABAS v1.7-pre5
          *
          * (Burillo's Automatic Resource Administration for BAses and Ships)
          *
@@ -71,7 +71,7 @@ namespace SpaceEngineers
          *
          */
 
-        const string VERSION = "1.7-pre4";
+        const string VERSION = "1.7-pre5";
 
         #region CONFIGVARS
         // configuration
